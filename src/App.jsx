4337fbc60c7793af39179d7ad7073da2b36e1438
CoderMasterypro <abdulrehmanabbasi5067794@gmail.com>
2023-12-11 11:49:22 -0800

    useefeect-windowsize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import BasicUseEffect from "./useEffect/BasicUseEffect";
+import Window from "./windowSize/Window";
+
+function App() {
+  return (
+    <>
+      <Window />
+      {/* <BasicUseEffect /> */}
+    </>
+  );
+}
+
+export default App;
